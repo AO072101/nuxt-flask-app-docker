67efdc5266240497c8a1bf85a091be630712c572
AOISORA072101 <skyblue092101@gmail.com>
2018-08-15 00:28:28 +0900

    add bulma

---
+++ app/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="title">{{ data.message }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData() {
+    let data = await axios.get('/api')
+    console.log(data)
+    return data.data
+  }
+}
+</script>
+
+<style lang="scss">
+.title {
+  // color: $info;
+  color: $primary;
+}
+</style>
